c65f98e51dab215ca9c025c2f1b8017a06202799
Siri2K <kanagalingham.sriram@gmail.com>
2022-08-21 20:06:21 -0400

    Connected Database to Login Form

---
+++ Shop/src/main/java/shop/connection/DbConnection.java
@@ -0,0 +1,28 @@
+package shop.connection;
+
+// Import Packages
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbConnection 
+{
+	// Setup Instances
+	private static Connection connect;
+	
+	// Setup Set & Get Functions
+	public static Connection getConnection()
+	{
+		try 
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/shop","root","DBA123");
+			System.out.println("Connected");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return connect;
+	}
+	
+}
